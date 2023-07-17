fc2a9926f35f81d4fb2e8197bf4048adf1421fcc
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-17 19:57:01 +0900

    Design: Global CSS 적용

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { router } from 'constants/routerInfo';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import 'global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
