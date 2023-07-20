aabd58c65e47697ef6217bffccf2d0d422428e2a
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-20 22:23:58 +0900

    Fix: Banner 이미지 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,10 @@
 import { KakaoMap } from 'components/KakaoMap';
-import { Banner, BannerImg, BannerTypo, Root } from './styled';
+import { Banner, BannerTypo, Root } from './styled';
 
 export const Main = () => {
   return (
     <Root>
       <Banner>
-        <BannerImg alt="banner_img" />
         <BannerTypo>배너 텍스트</BannerTypo>
       </Banner>
       <KakaoMap />
--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
-import childrenProtectImg from 'assets/images/children_protect.jpeg';
-
+import bannerImg from 'assets/images/banner.jpeg';
 export const Root = styled.div`
+  width: 100%;
+  height: 100%;
   position: relative;
   display: flex;
   flex-direction: column;
-  overflow: hidden;
+  justify-content: space-between;
   border: 1px solid red;
+  overflow: hidden;
 `;
 
 export const Banner = styled.div`
+  background-image: url(${bannerImg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 600px;
+  position: relative;
   border: 1px solid blue;
+  overflow: hidden;
 `;
-export const BannerImg = styled.img`
-  content: url(${childrenProtectImg});
-  width: 100%;
-  height: 100%;
-  margin: 0;
+
+export const BannerTypo = styled.p`
+  position: absolute;
+  top: 20px;
+  left: 30px;
+  color: white;
+  font-weight: 700;
+  font-size: 32px;
 `;
-export const BannerTypo = styled.p``;
