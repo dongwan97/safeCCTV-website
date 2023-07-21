3ba14e929f3f15121e04bd4ba984fcd43e468708
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-21 14:47:27 +0900

    Feat: commentInput 관련 기능 구현

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   CommentContainer,
   CommentInput,
@@ -11,13 +11,42 @@ import {
   ViewCount,
 } from './styled';
 import samplePostDetailData from 'constants/samplePostDetail.json';
+import sampleCommentList from 'constants/sampleComment.json';
 import { useParams } from 'react-router-dom';
+import { Comment } from 'components/Comment';
 
 export const PostDetail = () => {
   const { postId } = useParams();
+  const inputRef = useRef();
   const [postDetailData, setPostDetailData] = useState(samplePostDetailData.samplePostDetail[+postId]);
-  const [currentCommentList, setCurrentCommentList] = useState([]);
-
+  const [currentCommentList, setCurrentCommentList] = useState(sampleCommentList.commentList);
+  const [commentInput, setCommentInput] = useState('');
+  const onChange = ({ target: { value } }) => {
+    setCommentInput(value);
+  };
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      if (!e.shiftKey) {
+        e.preventDefault();
+        if (commentInput.length === 0) return;
+        inputRef.current.disabled = true;
+        setTimeout(() => {
+          inputRef.current.disabled = false;
+          inputRef.current.focus();
+        }, 0); //한국어 입력 오류 방지용 코드입니다.
+        setCurrentCommentList((prev) =>
+          prev.concat({
+            commentId: currentCommentList[currentCommentList.length - 1].commentId + 1,
+            content: commentInput,
+            commentOwnerName: '현재 로그인 한 사용자 닉네임',
+            date: '새 댓글 날짜',
+          })
+        );
+        //댓글 저장 api
+        setCommentInput('');
+      }
+    }
+  };
   useEffect(
     () => {},
     [
@@ -33,8 +62,12 @@ export const PostDetail = () => {
       <Date>{postDetailData.date}</Date>
       <ViewCount>{postDetailData.ViewCount}</ViewCount>
       <UserNickname>로그인한 사용자의 닉네임</UserNickname>
-      <CommentContainer>{/* comment.map */}</CommentContainer>
-      <CommentInput />
+      <CommentContainer>
+        {currentCommentList.map((comment) => (
+          <Comment {...comment} key={comment.commentId} />
+        ))}
+      </CommentContainer>
+      <CommentInput value={commentInput} onChange={onChange} onKeyDown={onKeyDown} ref={inputRef} />
     </Root>
   );
 };
