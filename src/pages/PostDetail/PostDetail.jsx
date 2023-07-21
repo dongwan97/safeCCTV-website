d5c4ca7d29122ec2d894f3adf708bb90d4792109
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-21 14:07:07 +0900

    Feat: PostDetail 페이지 추가

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,5 +1,39 @@
-import { Root } from './styled';
+import { useEffect, useState } from 'react';
+import {
+  CommentContainer,
+  CommentInput,
+  Content,
+  Date,
+  PostOwnerName,
+  Root,
+  Title,
+  UserNickname,
+  ViewCount,
+} from './styled';
+import samplePostDetailData from 'constants/samplePostDetail.json';
+import { useParams } from 'react-router-dom';
 
 export const PostDetail = () => {
-  return <Root>PostDetail</Root>;
+  const postId = useParams();
+  const [postDetailData, setPostDetailData] = useState(samplePostDetailData.samplePostDetail[+postId]);
+  const [currentCommentList, setCurrentCommentList] = useState([]);
+
+  useEffect(
+    () => {},
+    [
+      //api로 detail 정보 받아옵니다. 백엔드 완료후 react router loader 함수로 다시 구현
+    ]
+  );
+  return (
+    <Root>
+      <Title>{postDetailData.title}</Title>
+      <PostOwnerName>{postDetailData.postOwnerName}</PostOwnerName>
+      <Content>{postDetailData.content}</Content>
+      <Date>{postDetailData.date}</Date>
+      <ViewCount>{postDetailData.ViewCount}</ViewCount>
+      <UserNickname>로그인한 사용자의 닉네임</UserNickname>
+      <CommentContainer>{/* comment.map */}</CommentContainer>
+      <CommentInput />
+    </Root>
+  );
 };
--- src/pages/PostDetail/styled.js
+++ src/pages/PostDetail/styled.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
 export const Root = styled.div``;
+export const PostOwnerName = styled.p``;
+export const Title = styled.p`
+  font-weight: 800;
+`;
 export const Content = styled.div``;
 export const Date = styled.p``;
+export const ViewCount = styled.p``;
+export const UserNickname = styled.p``;
+
+export const CommentContainer = styled.div``;
+
+export const CommentInput = styled.textarea`
+  resize: none;
+`;
