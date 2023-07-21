d0d93e62c77bb2482340ff62349eb13875054f5f
wandong <122766043+dongwan97@users.noreply.github.com>
2023-07-21 18:27:20 +0900

    Feat: 서비스 소개 기능 추가

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,11 @@
 import './Landing.css';
 import car1 from 'assets/images/car.jpg';
 import crossboard1 from 'assets/images/crossboard1.jpg';
+import service1 from 'assets/images/service1.png';
+import service2 from 'assets/images/service2.png';
+import service3 from 'assets/images/service3.png';
+import service4 from 'assets/images/service4.png';
+
 import {
   Crossboard1,
   Root,
@@ -15,7 +20,19 @@ import {
   TopTypoContainer,
   Car1,
   LandingContainer,
+  ServicesContainer,
+  ServicesRow,
+  Service,
+  ServiceImg,
+  ServiceTypo,
+  ServiceTypoTop,
+  ServiceImgCar,
+  ContactContainer,
+  ContactTitle,
+  TopTypoText,
+  TopTypoMain,
 } from './styled';
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
   return (
@@ -24,8 +41,8 @@ export const Landing = () => {
 
       <LandingContainer>
         <TopTypoContainer>
-          <h2>We GO the Extra Mile for you</h2>
-          <div>need a lift? we can help</div>
+          <TopTypoMain>한테크 관제 플랫폼</TopTypoMain>
+          <TopTypoText>we try to solve Children Safe Zone problem</TopTypoText>
         </TopTypoContainer>
 
         <SecondContainer>
@@ -44,17 +61,79 @@ export const Landing = () => {
         {/* <img src={meeting5} className="meeting5"></img> */}
         <ThirdContainer>
           <ThirdTypoContainer>
-            <ThirdTypoTop>알면 좋은 서비스</ThirdTypoTop>
-            <ThirdTypoText>이런 서비스도</ThirdTypoText>
-            <ThirdTypoText>한번 써보세요</ThirdTypoText>
+            <ThirdTypoTop>한눈에 확인하기</ThirdTypoTop>
+            <ThirdTypoText>어린이 보호구역의 안전</ThirdTypoText>
+            <ThirdTypoText>저희 페이지에서 쉽게 확인하세요</ThirdTypoText>
           </ThirdTypoContainer>
-          <div className="service1">
-            <h1>service1</h1>
-            <p>service 설명</p>
-            <button>홈페이지 방문</button>
-          </div>
+
+          <ServicesContainer>
+            <ServicesRow>
+              <Service>
+                <ServiceImg src={service1} />
+                <ServiceTypoTop>안전도</ServiceTypoTop>
+                <ServiceTypo>
+                  어린이 보호구역의 안전도를
+                  <br />
+                  한눈에 쉽게 확인 할 수 있어요.
+                </ServiceTypo>
+                <Link to="/main" className="Link">
+                  더 알아보기
+                </Link>
+              </Service>
+
+              <Service>
+                <ServiceImgCar src={service2} />
+                <ServiceTypoTop>안전도</ServiceTypoTop>
+                <ServiceTypo>
+                  어린이 보호구역의 안전도를
+                  <br />
+                  한눈에 쉽게 확인 할 수 있어요.
+                </ServiceTypo>
+                <Link to="/main" className="Link">
+                  더 알아보기
+                </Link>
+              </Service>
+            </ServicesRow>
+
+            <ServicesRow>
+              <Service>
+                <ServiceImg src={service3} />
+                <ServiceTypoTop>안전도</ServiceTypoTop>
+                <ServiceTypo>
+                  어린이 보호구역의 안전도를
+                  <br />
+                  한눈에 쉽게 확인 할 수 있어요.
+                </ServiceTypo>
+                <Link to="/main" className="Link">
+                  더 알아보기
+                </Link>
+              </Service>
+              <Service>
+                <ServiceImg src={service4} />
+                <ServiceTypoTop>안전도</ServiceTypoTop>
+                <ServiceTypo>
+                  어린이 보호구역의 안전도를
+                  <br />
+                  한눈에 쉽게 확인 할 수 있어요.
+                </ServiceTypo>
+                <Link to="/main" className="Link">
+                  더 알아보기
+                </Link>
+              </Service>
+            </ServicesRow>
+          </ServicesContainer>
         </ThirdContainer>
       </LandingContainer>
+
+      <ContactContainer>
+        <ContactTitle>
+          한테크에서는
+          <br />
+          안전을 위한 더욱 다양한 고민을 합니다
+          <br />
+          저희와 접촉하여 더 많은 제품과 서비스를 만나보세요
+        </ContactTitle>
+      </ContactContainer>
     </Root>
   );
 };
