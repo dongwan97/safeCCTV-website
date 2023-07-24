719c3b185e34d9d1075096e19b87f5bcc7c32eaa
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-24 15:39:50 +0900

    Feat: Add COMMENT MAX, MIN WIDTH

--- src/constants/layout.js
+++ src/constants/layout.js
@@ -3,3 +3,5 @@ export const HEADER_MIN_WIDTH = 500;
 export const HEADER_HEIGHT = 50;
 export const BODY_MAX_WIDTH = 1440;
 export const BODY_MIN_WIDTH = 500;
+export const COMMENT_MAX_WIDTH = 500;
+export const COMMENT_MIN_WIDTH = 300;
