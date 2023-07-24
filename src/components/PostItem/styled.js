af8df291585e855e0cef7ab99233589d78086630
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-07-24 15:38:30 +0900

    Design: PostItem

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 
 export const Root = styled.div`
+  position: relative;
   border-bottom: 1px solid black;
+  box-sizing: border-box;
+  padding: 5px;
 `;
 
 export const TitleTypo = styled.p`
   cursor: pointer;
+  font-weight: 900;
+  width: fit-content;
 `;
 
-export const DateTypo = styled.p``;
+export const DateTypo = styled.p`
+  font-size: 12px;
+  color: #555555;
+`;
 
-export const ViewCount = styled.p``;
-
-export const PostTitle = styled.p`
-  text-align: center;
-  font-weight: 700;
-  font-size: 28px;
+export const ViewCount = styled.p`
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  color: #555555;
 `;
 
 export const Nickname = styled.p``;
