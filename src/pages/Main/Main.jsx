0ec313d314ba5d5d5e6af0cc07776417b6723db4
wandong <122766043+dongwan97@users.noreply.github.com>
2023-08-11 18:16:55 +0900

    Design : 메인페이지 디자인 요소 변경

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,25 @@
 import { KakaoMap } from 'components/KakaoMap';
-import { Banner, BannerTypo, BannerTypoWrapper, Root } from './styled';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import {
+  Banner,
+  BannerTypo,
+  BannerTypoWrapper,
+  MainImg,
+  MainService,
+  MainServiceContent,
+  MainServiceContentContainer,
+  MainServiceSmall,
+  MainServiceTitle,
+  Root,
+  Line,
+  ContactContainer,
+  ContactTitle,
+  ContactContent,
+  ServiceColor,
+  BannerTypoTitle,
+} from './styled';
 
 export const Main = () => {
   const navigate = useNavigate();
@@ -10,10 +28,24 @@ export const Main = () => {
     <Root>
       <Banner>
         <BannerTypoWrapper>
-          <BannerTypo>배너 텍스트</BannerTypo>
+          <BannerTypoTitle>안전관리의 </BannerTypoTitle>
+          <BannerTypoTitle>새로운 스탠다드</BannerTypoTitle>
+          <BannerTypo>한테크 실시간 관제 플랫폼</BannerTypo>
         </BannerTypoWrapper>
       </Banner>
+      <MainService>
+        <MainServiceTitle>service</MainServiceTitle>
+        <Line />
+        <MainServiceContent>
+          저희의 실시간 관제플랫폼은 관리해야 할 안전구역을 한눈에 확인 가능하게 도와주고
+          <br /> 직관적인 시각화를 통해 보다 쉽게 관리 할 수 있도록 도움을 드립니다.
+        </MainServiceContent>
+      </MainService>
       <KakaoMap />
+      <ContactContainer>
+        <ContactTitle>Contact Us</ContactTitle>
+        <ContactContent>문의사항이 있으시면 전화걸어 911 </ContactContent>
+      </ContactContainer>
     </Root>
   );
 };
--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import bannerImg from 'assets/images/banner.jpg';
+import bannerImg2 from 'assets/images/banner2.jpg';
+import mainBackground from 'assets/images/mainBackground.jpg';
+import contactImg from 'assets/images/mainBackground2.jpg';
 
 export const Root = styled.div`
   width: 100%;
@@ -9,25 +11,29 @@ export const Root = styled.div`
   flex-direction: column;
   justify-content: space-between;
   overflow: hidden;
-  padding-bottom: 100px;
 `;
 
 export const Banner = styled.div`
-  background: url(${bannerImg}) no-repeat center;
+  background: url(${bannerImg2}) no-repeat center;
   background-size: cover;
-  width: 100vw;
+  background-attachment: fixed;
+
+  width: 110vw;
   height: 600px;
   position: relative;
   overflow: hidden;
 `;
 
+export const BannerTypoTitle = styled.div`
+  color: white;
+  font-weight: bold;
+  font-size: 45px;
+`;
+
 export const BannerTypoWrapper = styled.div`
   position: absolute;
-  top: 100px;
-  left: 100px;
-  background-color: #75c2f6;
-  opacity: 0.9;
-  border-radius: 20px;
+  top: 90px;
+  left: 70px;
   padding: 20px;
 `;
 
@@ -35,4 +41,63 @@ export const BannerTypo = styled.p`
   color: white;
   font-weight: 700;
   font-size: 32px;
+  margin-top: 10px;
+`;
+
+export const MainService = styled.div`
+  width: 100vw;
+  background: url(${mainBackground});
+  height: 70vh;
+  background-size: cover;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
+export const MainServiceTitle = styled.p`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+`;
+export const Line = styled.hr`
+  width: 100px;
+  border: 1px solid white;
+`;
+
+export const MainServiceContent = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: white;
+  font-size: 20px;
+  text-align: center;
+`;
+
+// export const ServiceColor = styled.span`
+//   color: orange;
+// `;
+
+export const ContactContainer = styled.div`
+  background: url(${contactImg}) no-repeat center;
+  background-size: cover;
+  background-attachment: fixed;
+  width: 100vw;
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContactTitle = styled.p`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+`;
+export const ContactContent = styled.p`
+  color: white;
+  font-size: 20px;
 `;
