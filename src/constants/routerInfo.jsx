81d3a60d2377c422eea77b717a19c907780b083f
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-08-16 12:08:21 +0900

    Fix: PostDetail loader 함수 적용

--- src/constants/routerInfo.jsx
+++ src/constants/routerInfo.jsx
@@ -1,3 +1,5 @@
+import { checkComment } from 'api/comment/checkComment';
+import { findPost } from 'api/post/findPost';
 import { Bulletin } from 'pages/Bulletin';
 import { Landing } from 'pages/Landing';
 import { Main } from 'pages/Main';
@@ -19,7 +21,18 @@ export const router = createBrowserRouter([
     children: [
       { path: '/', element: <Landing /> },
       { path: '/main', element: <Main /> },
-      { path: '/post/:postId', element: <PostDetail /> },
+      {
+        path: '/post/:postId',
+        element: <PostDetail />,
+        loader: async ({ params }) => {
+          const postDetailData = await findPost(+params.postid);
+          const commentList = await checkComment();
+          return {
+            postDetailData: postDetailData,
+            commentList: commentList,
+          };
+        },
+      },
       { path: '/post/create', element: <PostCreate /> },
       { path: '/post', element: <Bulletin /> },
     ],
